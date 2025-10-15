8ce7c135d05734495ebb2216a0ed559c1f6b136f
GrzegorzT <144318154+GrzTer@users.noreply.github.com>
2025-10-15 09:23:10 +0200

    ZadanieEgzaminacyjne1-Virmox

---
+++ Zadanie Egzaminacyjne1 - Virmox/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Zadanie_Egzaminacyjne1___Virmox
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
